4673c90e68ded75bad558f953ea372fd7c4a797a
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-04-19 21:12:21 -0700

    added Navebar

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,34 @@
+import { Menu, Person, SearchOutlined, SendOutlined } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+export default function IndexPage() {
+  return (
+
+    <div >
+      <header className=' p-4 flex justify-between'>
+        <a href='' className='flex items-center ml-4'>
+          <SendOutlined className=' text-red-600 -rotate-90' style={{ fontSize: 50 }}></SendOutlined>
+          <span className='text-[24px] font-bold text-red-600'>Home Booking</span>
+        </a>
+        <div className='flex border border-gray-500 rounded-full items-center py-2 px-4  shadow-md shadow-gray-300'>
+          <div className=' border-r border-gray-300 pr-4'>Anywhere</div>
+
+          <div className=' border-r border-gray-300 pr-4 pl-4'>Any week</div>
+          <div className=' border-r border-gray-300 pr-4 pl-4'>Add guests</div>
+
+          <div className=' pl-2'>
+            <button className='  bg-primary text-white p-1 rounded-full '>
+              <SearchOutlined style={{ fontSize: 25 }}></SearchOutlined>
+            </button>
+          </div>
+        </div>
+        <Link to={'/login'} className='flex border border-gray-500 rounded-full p500 items-center py-2 px-4 gap-5'>
+          <Menu></Menu>
+          <div className=' bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden '>
+            <Person className=' relative top-1 '></Person>
+          </div>
+        </Link>
+      </header>
+    </div>
+
+  );
+}
